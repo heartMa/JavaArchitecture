a81f3136ae26575f88d67ec62f88752fc5cbc9ac
891091835 <891091835@qq.com>
2018-11-03 11:12:48 +0800

    V002分模块、分工程管理

---
+++ JavaArchitectureGoodsMgr/src/main/java/com/w3cjava/JavaArchitecture/GoodsMgr/App.java
@@ -0,0 +1,13 @@
+package com.w3cjava.JavaArchitecture.GoodsMgr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
