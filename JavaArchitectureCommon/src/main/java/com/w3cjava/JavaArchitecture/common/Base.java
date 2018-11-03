e4b6e20d88bb38a36f61ab7a30f259c5f4b23b69
891091835 <891091835@qq.com>
2018-11-03 12:28:03 +0800

    V003多模块多Web应用合并War包

---
+++ JavaArchitectureCommon/src/main/java/com/w3cjava/JavaArchitecture/common/Base.java
@@ -0,0 +1,7 @@
+package com.w3cjava.JavaArchitecture.common;
+
+public class Base {
+	public void base(){
+		System.out.println("base");
+	}
+}	
---
+++ JavaArchitectureGoodsMgr/src/main/java/com/w3cjava/JavaArchitecture/GoodsMgr/Good.java
@@ -0,0 +1,11 @@
+package com.w3cjava.JavaArchitecture.GoodsMgr;
+
+/**
+ * Hello world!
+ * 
+ */
+public class Good {
+	public void good() {
+		System.out.println("good");
+	}
+}
