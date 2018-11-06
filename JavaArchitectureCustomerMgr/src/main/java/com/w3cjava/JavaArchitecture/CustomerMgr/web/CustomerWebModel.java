91e4db1626ee2deee4402825bef466cfb50025b1
891091835 <891091835@qq.com>
2018-11-06 10:06:52 +0800

    添加springmvc 实现基础的增删改查

---
+++ JavaArchitectureCustomerMgr/src/main/java/com/w3cjava/JavaArchitecture/CustomerMgr/web/CustomerWebModel.java
@@ -0,0 +1,9 @@
+package com.w3cjava.JavaArchitecture.CustomerMgr.web;
+
+import com.w3cjava.JavaArchitecture.common.web.BaseWebModel;
+
+
+public class CustomerWebModel extends BaseWebModel{	
+	
+	
+}
